e900534c5f7d0f7b0142d25579149c6c0e07e174
Tharun Raj <tharunraj2262004@gmail.com>
2024-10-04 13:03:26 +0530

    Commit

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -11,36 +11,43 @@ const Navbar = () => {
 
   return (
     <div>
-      {/* Hamburger Icon */}
-      <img
-        src={hamburgerIcon}
-        alt="Menu"
-        className="fixed top-5 right-5 w-8 h-8 cursor-pointer z-50"
-        onClick={toggleNavbar}
-      />
+      {/* Conditionally render Hamburger Icon when navbar is closed */}
+      {!isOpen && (
+        <img
+          src={hamburgerIcon}
+          alt="Menu"
+          className="fixed top-5 right-5 w-14 h-14 cursor-pointer z-50"
+          onClick={toggleNavbar}
+        />
+      )}
 
       {/* Navbar Overlay */}
-      {isOpen && (
-        <div className="fixed top-0 right-0 w-64 h-full bg-[#f4e0c6] shadow-lg flex flex-col p-5 z-40 transition-transform duration-300">
-          <img
-            src={closeIcon}
-            alt="Close"
-            className="absolute top-5 right-5 w-8 h-8 cursor-pointer"
-            onClick={toggleNavbar}
-          />
-          <nav className="mt-12 flex flex-col space-y-4">
-            <a href="/home" className="text-black text-lg font-semibold">
-              Home
-            </a>
-            <a href="/events" className="text-black text-lg font-semibold">
-              Events
-            </a>
-            <a href="/contact" className="text-black text-lg font-semibold">
-              Contact Us
-            </a>
-          </nav>
-        </div>
-      )}
+      <div
+        className={`fixed top-0 right-0 w-64 h-full bg-[#f4e0c6] shadow-lg flex flex-col p-5 z-40 transform transition-transform duration-300 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        {/* Close Icon */}
+        <img
+          src={closeIcon}
+          alt="Close"
+          className="absolute top-5 right-5 w-14 h-14 cursor-pointer"
+          onClick={toggleNavbar}
+        />
+
+        {/* Navigation Links */}
+        <nav className="mt-12 flex flex-col space-y-6 pt-5">
+          <a href="/" className="textnav text-black text-lg font-semibold">
+            Home
+          </a>
+          <a href="/events" className="textnav text-black text-lg font-semibold">
+            Events
+          </a>
+          <a href="/contact" className="textnav text-black text-lg font-semibold">
+            Contact Us
+          </a>
+        </nav>
+      </div>
     </div>
   );
 };
